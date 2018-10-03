9ae0fa7716a60a0906fe88afb95cf16fb938cbcd
Vincent Kammerer <vkammerer@gmail.com>
2018-10-03 10:23:59 -0400

    Adds static assets

---
+++ docs/precache-manifest.04b18e6fc577fa01f5933e3ef6352fe0.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "03966f8262da2c2fe9ce",
+    "url": "/react-target-portal-example/static/js/runtime~main.03966f82.js"
+  },
+  {
+    "revision": "acf913c407eed163673b",
+    "url": "/react-target-portal-example/static/js/main.acf913c4.chunk.js"
+  },
+  {
+    "revision": "78f6cd5809eca47c902b",
+    "url": "/react-target-portal-example/static/js/1.78f6cd58.chunk.js"
+  },
+  {
+    "revision": "61943d952324b4ce47b2fb3e19edbce1",
+    "url": "/react-target-portal-example/index.html"
+  }
+];
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.98d13e7033d87a50f5c6db9ba02095f1.js"
+  "/react-target-portal-example/precache-manifest.04b18e6fc577fa01f5933e3ef6352fe0.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/react-target-portal-example/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
