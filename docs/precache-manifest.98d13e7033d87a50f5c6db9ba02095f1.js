200b1eeea599d5855f3c500f6986596f04a431c8
Vincent Kammerer <vkammerer@gmail.com>
2018-10-03 09:47:55 -0400

    Adds static assets

---
+++ docs/precache-manifest.98d13e7033d87a50f5c6db9ba02095f1.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "acf913c407eed163673b",
+    "url": "/static/js/main.acf913c4.chunk.js"
+  },
+  {
+    "revision": "78f6cd5809eca47c902b",
+    "url": "/static/js/1.78f6cd58.chunk.js"
+  },
+  {
+    "revision": "e5ed17234c4d58e5f281cf9443edcd20",
+    "url": "/index.html"
+  }
+];
